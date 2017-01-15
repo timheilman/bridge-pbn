2c6e599ef399c5954ed766236aa01c728b6440f3
Tim Heilman <tim@dv8.org>
2017-01-15 15:54:18 -0800

    Implement pbn deal string format parsing.

--- lib/bridge/pbn.rb
+++ lib/bridge/pbn.rb
@@ -1,5 +1,19 @@
 module Bridge
+  # see http://www.tistis.nl/pbn/
   class Pbn
+    # see spec from url at top, section 3.4.11
+    def self.deal(pbn_deal_string)
+      pbn_deal_string.split(':').reduce(-1) do |startingHandIndex, firstOrHands|
+        if startingHandIndex == -1
+          hand_index_for_pgn_character(firstOrHands)
+        else
+          firstOrHands.split(' ').reduce([]) do |memo, pbn_hand_string|
+            memo << (pbn_hand_string == '-' ? nil : Pbn.hand(pbn_hand_string))
+          end.rotate(startingHandIndex)
+        end
+      end
+    end
+
     def self.hand(pbn_hand_string)
       pbn_hand_string.split(/\./).reduce([]) do |memo, hand|
         suit = Bridge::Strain.suits[-memo.length-1]
@@ -8,6 +22,21 @@ module Bridge
           innermemo << Bridge::Card.for(ranks: [Bridge::Rank.forLetter(rankOfSuit)], suits: [suit])
         end
       end.flatten
+    end
+
+    private
+
+    def self.hand_index_for_pgn_character(firstPosition)
+      case firstPosition
+        when 'N'
+          0
+        when 'E'
+          1
+        when 'S'
+          2
+        when 'W'
+          3
+      end
     end
   end
 end

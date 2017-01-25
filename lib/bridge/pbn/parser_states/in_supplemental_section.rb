5ad216cd20477920003ffee9d9046e7247933831
Tim Heilman <tim@dv8.org>
2017-01-24 18:52:03 -0800

    Eliminate last loop requiring iterator rather than block with each_char usage in game_parser.

--- lib/bridge/pbn/parser_states/in_supplemental_section.rb
+++ lib/bridge/pbn/parser_states/in_supplemental_section.rb
@@ -5,27 +5,27 @@ module Bridge
       include Bridge::Pbn::ParserConstants
       include Bridge::Pbn::ParserState
 
+      def post_initialize
+        @section = ''
+      end
+
       def process_chars
         # we must return the section untokenized, since newlines hold special meaning for ;-comments
         # and are permitted to appear in Auction and Play sections
-        section_in_entirety = ''
-        until parser.state.done? # todo: fix this demeter violation
-          case parser.cur_char
-            when SECTION_INCLUDE_COMMENTS_BUGGY_HACK # curly braces and semicolons must be allowed through for commentary in play and auction blocks
-              section_in_entirety << parser.cur_char
-              parser.inc_char
-            when OPEN_BRACKET
-              parser.add_section(section_in_entirety) unless section_in_entirety.empty?
-              parser.state = BeforeTagName.new(parser)
-              parser.inc_char
-              return
-            else
-              parser.raise_exception
-          end
+        case parser.cur_char
+          when SECTION_INCLUDE_COMMENTS_BUGGY_HACK # curly braces and semicolons must be allowed through for commentary in play and auction blocks
+            @section << parser.cur_char
+            parser.inc_char
+          when OPEN_BRACKET
+            parser.add_section(@section) unless @section.empty?
+            parser.state = BeforeTagName.new(parser)
+            parser.inc_char
+            return
+          else
+            parser.raise_exception
         end
-        parser.add_section(section_in_entirety) unless section_in_entirety.empty?
+        parser.add_section(@section) unless @section.empty?
       end
-
     end
   end
 end

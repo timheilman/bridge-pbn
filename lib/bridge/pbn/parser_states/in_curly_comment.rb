bf5607adae7d83ebb6880428729ba4f37af4ff5b
Tim Heilman <tim@dv8.org>
2017-01-25 15:25:26 -0800

    Tighten curly comment contents to spec.

--- lib/bridge/pbn/parser_states/in_curly_comment.rb
+++ lib/bridge/pbn/parser_states/in_curly_comment.rb
@@ -9,12 +9,17 @@ module Bridge
         @comment = ''
       end
 
-      def process_char char
-        case char
+      def process_char(char)
+        char_in_latin_1 = char.encode(Encoding::ISO_8859_1)
+        case char_in_latin_1
           when CLOSE_CURLY
             next_state.add_comment(@comment)
             return next_state
           else
+            case char_in_latin_1.ord
+              when 0..8, 12, 13..31, 127..159
+                parser.raise_error "disallowed character in PBN files, decimal code: #{char_in_latin_1.ord}"
+            end
             @comment << char
             return self
         end
---
+++ spec/lib/pbn/parser_states/in_curly_comment_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+RSpec.describe Bridge::Pbn::InCurlyComment do
+  describe('#process_char') do
+    let(:parser) { double }
+    let(:described_object) { described_class.new(parser) }
+    context('with é provided in UTF-8 encoding') do
+      let(:char) { 'é' }
+      it('should allow the character') do
+        expect(char.encoding).to be Encoding::UTF_8
+        expect(described_object.process_char(char)).to be(described_object)
+      end
+    end
+    context('with a form feed provided in UTF-8 encoding') do
+      let(:char) { "\u000c" }
+      it('should disallow the ASCII control character') do
+        expect(char.encoding).to be Encoding::UTF_8
+        expect { described_object.process_char(char) }.to raise_error(/.*disallowed.*: 12/)
+      end
+    end
+    context('with a non-ASCII control character from ISO 8859/1') do
+      let(:char) { 143.chr(Encoding::ISO_8859_1).encode(Encoding::UTF_8) }
+      it('should disallow the character') do
+        expect(char.encoding).to be Encoding::UTF_8
+        expect { described_object.process_char(char) }.to raise_error(/.*disallowed.*: 143/)
+      end
+    end
+
+
+  end
+end

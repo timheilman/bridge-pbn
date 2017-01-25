33cbe69e507100bba3a53d2f04517316ac658df5
Tim Heilman <tim@dv8.org>
2017-01-25 09:30:11 -0800

    Bring in_string coverage to 100

--- lib/bridge/pbn/parser_states/in_string.rb
+++ lib/bridge/pbn/parser_states/in_string.rb
@@ -26,8 +26,7 @@ module Bridge
               parser.add_tag_item(@string) # todo: fix this for strings in supplemental sections
               return next_state
             end
-          else
-            raise_error
+            #cases are exhaustive; no else needed
         end
         self
       end

d9ce8729ead04b83885c5b2d217909829bb2c631
Tim Heilman <tim@dv8.org>
2017-01-24 18:01:10 -0800

    Eliminate a while loop by providing an instance variable.

--- lib/bridge/pbn/parser_states/in_tag_name.rb
+++ lib/bridge/pbn/parser_states/in_tag_name.rb
@@ -5,14 +5,19 @@ module Bridge
       include Bridge::Pbn::ParserConstants
       include Bridge::Pbn::ParserDelegate
 
+      def post_initialize
+        @tag_name = ''
+      end
+
       def process_chars
-        tag_name = ''
-        until parser.state.done? || parser.cur_char !~ ALLOWED_NAME_CHARS # todo: fix this demeter violation
-          tag_name << parser.cur_char
-          parser.inc_char
+        case parser.cur_char
+          when ALLOWED_NAME_CHARS
+            @tag_name << parser.cur_char
+          else
+            parser.add_tag_item(@tag_name)
+            parser.state = BeforeTagValue.new(parser, self)
         end
-        parser.add_tag_item tag_name
-        parser.state = BeforeTagValue.new(parser) unless parser.state.done? # todo: fix this demeter violation
+        parser.inc_char
       end
     end
   end

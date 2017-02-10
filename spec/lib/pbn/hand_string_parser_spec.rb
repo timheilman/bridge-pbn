0abe3df022e4d3e14ff6c9d847aff45194b5c6b8
Tim Heilman <tim@dv8.org>
2017-02-10 10:29:26 -0800

    Explicitly show all four suits in a hand, and include T to represent a ten.

--- spec/lib/pbn/hand_string_parser_spec.rb
+++ spec/lib/pbn/hand_string_parser_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe Bridge::Pbn::HandStringParser do
   describe '.hand' do
     let(:described_object) { Bridge::Pbn::HandStringParser.new(hand) }
     context 'provided a valid PBN hand string' do
-      let(:hand) { '.63.AKQ987.A9732' }
+      let(:hand) { '7.63.AKQJT.A9732' }
       it 'yields the proper suits and ranks' do
         expect { |block| described_object.yield_cards(&block) }.to yield_successive_args(
+                                                                      {suit: 'S', rank: '7'},
                                                                       {suit: 'H', rank: '6'},
                                                                       {suit: 'H', rank: '3'},
                                                                       {suit: 'D', rank: 'A'},
                                                                       {suit: 'D', rank: 'K'},
                                                                       {suit: 'D', rank: 'Q'},
-                                                                      {suit: 'D', rank: '9'},
-                                                                      {suit: 'D', rank: '8'},
-                                                                      {suit: 'D', rank: '7'},
+                                                                      {suit: 'D', rank: 'J'},
+                                                                      {suit: 'D', rank: 'T'},
                                                                       {suit: 'C', rank: 'A'},
                                                                       {suit: 'C', rank: '9'},
                                                                       {suit: 'C', rank: '7'},

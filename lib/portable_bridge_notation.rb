41140ca55240b328f7c3b2eb19ea0ff712e0fa5b
Tim Heilman <tim@dv8.org>
2017-02-16 09:18:20 -0800

    Delete done portion of a todo.

--- lib/portable_bridge_notation.rb
+++ lib/portable_bridge_notation.rb
@@ -1,5 +1,4 @@
 # TODO: learn about ruby documentation format and add one here, to eliminate rubocop warning
-# as well as how to turn it off for Internal:: classes
 module PortableBridgeNotation
   autoload :Importer, File.expand_path('../portable_bridge_notation/importer', __FILE__)
 end

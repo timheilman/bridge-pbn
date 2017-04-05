f983bff33414124a0658cb5afe561fa400dcdb0a
Tim Heilman <tim@dv8.org>
2017-04-05 12:10:19 -0700

    Small redundancy elimination

--- lib/portable_bridge_notation/internals/io_parser.rb
+++ lib/portable_bridge_notation/internals/io_parser.rb
@@ -16,13 +16,14 @@ module PortableBridgeNotation
         return enum_for(:each_game_string) unless block_given?
         self.record = ''
         self.comment_is_open = false
+        self.block = block
         # unfortunately the spec requires Latin-1.  Internal processing, since record's encoding is UTF-8,
         # will be in UTF-8.  Export to PBN will need to set the encoding on the export IO back to ISO_8859_1
         io.set_encoding(Encoding::ISO_8859_1)
         io.each do |line|
-          process_line line, &block
+          process_line line
         end
-        yield record unless record.empty?
+        yield_record
       end
 
       private
@@ -30,19 +31,24 @@ module PortableBridgeNotation
       attr_reader :io
       attr_accessor :record
       attr_accessor :comment_is_open
+      attr_accessor :block
 
-      def process_line(line, &block)
+      def process_line(line)
         if comment_is_open
           record << line
         elsif line =~ PBN_ESCAPED_LINE
           # potential site for processing of future directives in exported PBN files from this project
         elsif line =~ SEMI_EMPTY_LINE
-          block.yield record
-          self.record = ''
+          yield_record
         else
           record << line
         end
         self.comment_is_open = comment_open_after_eol? line, comment_is_open
+      end
+
+      def yield_record
+        block.yield record unless record.empty?
+        self.record = ''
       end
     end
   end

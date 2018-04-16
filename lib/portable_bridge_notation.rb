3a64bbeb744016d2d2933d29f8d5ace85bdb9810
Tim Heilman <tim@dv8.org>
2018-04-16 14:28:44 -0700

    Also allow autoload of Game.

--- lib/portable_bridge_notation.rb
+++ lib/portable_bridge_notation.rb
@@ -5,5 +5,6 @@ module PortableBridgeNotation
   # Please see ../../Readme.md
   module Api
     autoload :Importer, File.expand_path('../portable_bridge_notation/api/importer', __FILE__)
+    autoload :Game, File.expand_path('../portable_bridge_notation/api/game', __FILE__)
   end
 end
